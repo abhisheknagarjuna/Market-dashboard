065afb761133b33a39aa0e3cf9271ffee5cde158
Abhishek Nagarjuna <abhisheknagarjuna@users.noreply.github.com>
2016-03-31 19:21:07 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,10 +1,24 @@
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
+shinyServer(function(input, output) {
+
   
-  output$plot1 <- renderPlot({
-    
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+  output$trinplot <- renderDygraph({
+    print("Hello")
+    library(dygraphs)
+    lungDeaths <- cbind(mdeaths, fdeaths)
+    dygraph(lungDeaths)
   })
-}
+    
+    output$text1 <- renderText({ 
+      "You have selected this"
+    })
+    
+#     finalData <- xts(data$trin, as.POSIXct(data$time, format="%m/%d/%Y"))
+#     #      finalData <- xts(plotData[,parameters], as.POSIXct(plotData$Time, format="%d-%m-%Y %H:%M"), tz = TZ)      
+#     dygraph(finalData, main = "", xlab = "Date") %>% 
+#       #       dygraph(finalData, main = "Chiller Playback", xlab = "Timestamp" ,height = "1000px") %>%
+#       #           dySeries(parametersY2Axis, axis = 'y2') %>%
+#       dyRangeSelector() 
+
+
+  
+})
