0955c838d584dfb487eaa63bba2eaf2740351788
Abhishek Nagarjuna <abhisheknagarjuna@users.noreply.github.com>
2016-03-31 19:22:05 +0530

    Update global.R

--- global.R
+++ global.R
@@ -1,5 +1,22 @@
+library(shiny)
+library(shinydashboard)
+library(reshape2)
+library(DT)
+library(dplyr)
+library(dygraphs)
+library(xts)
+library(stringi)
+library(ggplot2)
 setwd("D:/Stock Analytics")
-data = read.csv("s&p2016.csv",stringsAsFactors=F)
-str(data)
-# data$prev_close = c(data$Close[1],data$Close[1:nrow(data)])
-# data$Advance = data$Close - data$prev_close
+data = read.csv("sandp_complete_data.csv",stringsAsFactors=F)
+#str(data)
+
+#TRIN
+data$trin = (data$Adv/data$Dec)/(data$Adv.Vol/data$Dec.Vol)
+
+#A/D LINE
+data$netadv = data$Adv - data$Dec
+head(data$netadv)
+data$netadv[is.na(data$netadv)] <-0
+
+data$adline = c(data$netadv[1],rep(NULL,(nrow(data)-1)))
